b77e4bb3d23e7206054f48fa8f19cdf4ac85106d
Matt Miermans <m.miermans@gmail.com>
2026-07-06 13:11:31 +0200

    refactor(HNT-2086): extract the pubsub client into its own package

--- packages/crawl-common/src/index.ts
+++ packages/crawl-common/src/index.ts
@@ -9,21 +9,6 @@ export type {
   UpdateApprovedCorpusItemInput,
   UpdateApprovedCorpusItemResponse,
 } from './corpus-api/index.js';
-export {
-  initPubSubClient,
-  startSubscriber,
-  publishMessage,
-  flushTopics,
-  shutdownPubSub,
-  sentryPubSubErrorHandler,
-} from './pubsub/index.js';
-export type {
-  PubSubClientOptions,
-  SubscriberOptions,
-  SubscriberController,
-  SubscriberErrorContext,
-  MessageHandler,
-} from './pubsub/index.js';
 export type {
   CorpusItem,
   CrawlArticleMessage,
